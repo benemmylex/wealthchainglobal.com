83f5bf776689a19c98e54c46ed1f5f28adc8f80e
benemmylex <164938631+benemmylex@users.noreply.github.com>
2025-10-02 11:18:28 +0000

    test

--- app/account/card.php
+++ app/account/card.php
@@ -12,11 +12,11 @@ ini_set('display_errors', 1);
         <div class="card shadow-4-strong border border-1 border-primary my-3">
             <div class="card-body">
                 <h5 class="mb-3">Card Types</h5>
-                <iframe src="cards_template" style="width:99%; height: 400px; border: none;"></iframe>
+<!--                 <iframe src="cards_template" style="width:99%; height: 400px; border: none;"></iframe>
                 <div class="text-center">
                     <button id="orderCardBtn" class="btn btn-success btn-flat btn-lg mt-4"><i
                             class="fa fa-download"></i> Order Card</button>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
